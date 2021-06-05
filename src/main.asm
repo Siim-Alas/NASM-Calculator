7dc7db46b16620978611268c92006f6f6434c2f9
Siim-Alas <siim.alas@outlook.com>
2021-06-05 12:36:03 +0300

    Added parsestring macro

--- src/main.asm
+++ src/main.asm
@@ -10,23 +10,19 @@ _start:	pop r8			; get the number of command-line arguments
 	cmp r8, 2		; program path + the expression string
 	jne ERR_INVALID_NUMBER_OF_CL_ARGS
 
-	pop rsi			; ignore the path to the executable
-	pop rsi			; get a pointer to the start of the expression
-	strlen rsi, rdx		; get the length of the expression string
+	pop r8			; ignore the path to the executable
+	pop r8			; get a pointer to the start of the expression
+	strlen r8, r9		; get the length of the expression string
 
 	%ifdef DEBUG
-		push rsi
-		push rdx
-
 		write_to_stdout debug_input_string_was_msg, \
 			debug_input_string_was_msg_len
-
-		pop rdx
-		pop rsi
-
-		write_to_stdout rsi, rdx
+		write_to_stdout r8, r9
 		write_to_stdout nl, 1
 	%endif
+
+	add r8, r9
+	parseint r8, r10
 
 	mov rax, SYS_EXIT
 	mov rdi, 0
