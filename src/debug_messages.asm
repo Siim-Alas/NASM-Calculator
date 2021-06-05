8c11faf00f2d8d8daf7c3178bc2f36dcca8dd9e2
Siim-Alas <siim.alas@outlook.com>
2021-06-05 09:11:32 +0300

    Added debug printout of the input string

---
+++ src/debug_messages.asm
@@ -0,0 +1,6 @@
+
+debug_input_string_was_msg: \
+db "DEBUG: The input string was: "
+debug_input_string_was_msg_len: \
+equ $ - debug_input_string_was_msg
+
--- src/main.asm
+++ src/main.asm
@@ -9,8 +9,23 @@ _start:	pop r8			; get the number of command-line arguments
 	cmp r8, 2		; program path + the expression string
 	jne ERR_INVALID_NUMBER_OF_CL_ARGS
 
+	pop rsi			; ignore the path to the executable
 	pop rsi			; get a pointer to the start of the expression
-	strlen rdx, rsi		; get the length of the expression string
+	strlen rsi, rdx		; get the length of the expression string
+
+	%ifdef DEBUG
+		push rsi
+		push rdx
+
+		write_to_stdout debug_input_string_was_msg, \
+			debug_input_string_was_msg_len
+
+		pop rdx
+		pop rsi
+
+		write_to_stdout rsi, rdx
+		write_to_stdout nl, 1
+	%endif
 
 	mov rax, SYS_EXIT
 	mov rdi, 0
@@ -20,5 +35,9 @@ _start:	pop r8			; get the number of command-line arguments
 
 
 	section .data
+%ifdef DEBUG
+nl:	db 0x0A
+	%include "debug_messages.asm"
+%endif
 %include "error_messages.asm"
 
